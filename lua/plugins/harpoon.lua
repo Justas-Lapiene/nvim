987d62989b104252f47d1c763571c215d64ea974
Justas <justas.lapiene@vinted.com>
2024-09-08 20:34:02 +0300

    hide harpoon keybinds on whichkey menu

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,86 +5,87 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("harpoon"):setup()
+      local wk = require("which-key")
+      wk.add({
+        {
+          "<leader>a",
+          function()
+            require("harpoon"):list():add()
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>h",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>1",
+          function()
+            require("harpoon"):list():select(1)
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>2",
+          function()
+            require("harpoon"):list():select(2)
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>3",
+          function()
+            require("harpoon"):list():select(3)
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>4",
+          function()
+            require("harpoon"):list():select(4)
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>5",
+          function()
+            require("harpoon"):list():select(5)
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>6",
+          function()
+            require("harpoon"):list():select(6)
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>7",
+          function()
+            require("harpoon"):list():select(7)
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>8",
+          function()
+            require("harpoon"):list():select(8)
+          end,
+          hidden = true,
+        },
+        {
+          "<leader>9",
+          function()
+            require("harpoon"):list():select(9)
+          end,
+          hidden = true,
+        },
+      })
     end,
-    keys = {
-      {
-        "<leader>a",
-        function()
-          require("harpoon"):list():add()
-        end,
-        desc = "harpoon file",
-      },
-      {
-        "<leader>h",
-        function()
-          local harpoon = require("harpoon")
-          harpoon.ui:toggle_quick_menu(harpoon:list())
-        end,
-        desc = "harpoon quick menu",
-      },
-      {
-        "<leader>1",
-        function()
-          require("harpoon"):list():select(1)
-        end,
-        desc = "harpoon to file 1",
-      },
-      {
-        "<leader>2",
-        function()
-          require("harpoon"):list():select(2)
-        end,
-        desc = "harpoon to file 2",
-      },
-      {
-        "<leader>3",
-        function()
-          require("harpoon"):list():select(3)
-        end,
-        desc = "harpoon to file 3",
-      },
-      {
-        "<leader>4",
-        function()
-          require("harpoon"):list():select(4)
-        end,
-        desc = "harpoon to file 4",
-      },
-      {
-        "<leader>5",
-        function()
-          require("harpoon"):list():select(5)
-        end,
-        desc = "harpoon to file 5",
-      },
-      {
-        "<leader>6",
-        function()
-          require("harpoon"):list():select(6)
-        end,
-        desc = "harpoon to file 6",
-      },
-      {
-        "<leader>7",
-        function()
-          require("harpoon"):list():select(7)
-        end,
-        desc = "harpoon to file 7",
-      },
-      {
-        "<leader>8",
-        function()
-          require("harpoon"):list():select(8)
-        end,
-        desc = "harpoon to file 8",
-      },
-      {
-        "<leader>9",
-        function()
-          require("harpoon"):list():select(9)
-        end,
-        desc = "harpoon to file 9",
-      },
-    },
   },
 }
